500c27fd4e6a240b3d84d59fbf9d85106ef92f92
Saburjon Safarov <saburjonsafarov2002@gmail.com>
2022-01-14 23:37:25 +0500

    vyostka fragmentov i activity_main.xml

--- app/src/main/java/tj/kpittu/news/view/MainActivity.kt
+++ app/src/main/java/tj/kpittu/news/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package tj.kpittu.news
+package tj.kpittu.news.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tj.kpittu.news.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/tj/kpittu/news/vm/ProfileFragmentViewModel.kt
@@ -0,0 +1,4 @@
+package tj.kpittu.news.vm
+
+class ProfileFragmentViewModel {
+}
